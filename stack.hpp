0224b35bbb83016e135211a1ffe0b42f0c80f8f6
hassansaadfr <saadaoui.hassan@laposte.net>
2021-11-26 13:46:57 +0100

    vector, stack tested and done

--- stack.hpp
+++ stack.hpp
@@ -49,7 +49,6 @@ namespace ft
 		protected:
 			container_type	c;
 	};
-
 	template< class T, class Container >
 	bool operator==( const stack<T,Container>& lhs, const stack<T,Container>& rhs ) { return lhs.c == rhs.c; };
 	template< class T, class Container >
